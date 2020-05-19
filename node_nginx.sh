bea7874172abd00591c2537cadfebb1d46258f2a
PhoenixNodes <53389469+phoenixnodes@users.noreply.github.com>
2020-05-19 05:15:15 -0700

    Update node_nginx.sh

--- node_nginx.sh
+++ node_nginx.sh
@@ -7,12 +7,12 @@ echo "server {
    listen 80 default_server;
    server_name _;
 
-   root /var/www/default; 
+   root /var/www/phxno.de; 
 
-}" > /etc/nginx/sites-available/default
+}" > /etc/nginx/sites-available/phxno.de
 
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/phxno.de /etc/nginx/sites-enabled/
 
-git clone -b node-webpage https://github.com/phoenixnodes/websites.git /var/www/default
+git clone -b node-webpage https://github.com/phoenixnodes/websites.git /var/www/phxno.de
 
 service nginx restart
