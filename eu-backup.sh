16417f14fac3aad7108d790db2ae2adb780c89a3
PhoenixNodes <53389469+phoenixnodes@users.noreply.github.com>
2020-08-15 23:50:20 -0700

    Create eu-backup.sh

---
+++ eu-backup.sh
@@ -0,0 +1,10 @@
+datecc=$(date '+%d-%m-%Y-%H-%M-%S');
+backupName=backup-$HOSTNAME-$datecc.zip
+
+zip -r  /srv/backups/$backupName /srv/daemon-data/*
+
+aws s3 cp /srv/backups/$backupName s3://phxnode.backups/$backupName --endpoint-url=https://s3.eu-central-1.wasabisys.com
+
+rm /srv/backups/$backupName
+
+echo "Done"
